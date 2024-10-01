f9497019642771c30aa638ecc2cd1d11ae85aa5e
OwenZhaoStudent <owenz19@nycstudents.net>
2024-10-01 17:13:55 -0400

    df

--- Zhao_Owen_Lab4.cpp
+++ Zhao_Owen_Lab4.cpp
@@ -10,47 +10,58 @@ using namespace std;
 
 int main() {
 
-    double Grape = 15;
-    double Strawberry = 7;
-    double Blueberry = 14;
-    double Grapefruit = 18;
-    double Rasberry = 5;
-    double SugarLimit;
-    double Cups;
-    double Math;
+    int Grape = 15;
+    int Strawberry = 7;
+    int Blueberry = 14;
+    int Grapefruit = 18;
+    int Rasberry = 5;
+    int Sugar;
+    int Guess;
+    int Guess2;
     string Fruit;
 
-    cout << "What is your sugar limit for today? ";
-    cin >> SugarLimit;
-
-    cout << "How many cups of this fruit are you trying to eat? ";
-    cin >> Cups;
-
+    cout << "In this game you will guess how much sugar is in a fruit!";
+    cout << endl<< endl;
     cout << "Pick a fruit: Grape, Strawberry, Blueberry, Grapefruit, Rasberry ";
     cin >> Fruit;
 
-    if(Fruit == "Grape")
-        Math = Cups * Grape;
+    if (Fruit == "Grape")
+        Sugar = Grape;
 
-    if(Fruit == "Strawberry")
-        Math = Cups * Strawberry;
+    if (Fruit == "Strawberry")
+        Sugar = Strawberry;
 
-    if(Fruit == "Blueberry")
-        Math = Cups * Blueberry;
+    if (Fruit == "Blueberry")
+        Sugar = Blueberry;
 
-    if(Fruit == "Grapefruit")
-        Math = Cups * Grapefruit;
+    if (Fruit == "Grapefruit")
+        Sugar = Grapefruit;
 
-    if(Fruit == "Rasberry")
-        Math = Cups * Rasberry;
+    if (Fruit == "Rasberry")
+        Sugar = Rasberry; 
 
-        while (Math > SugarLimit)
-            cout << "The amount of food you're going to eat is over your sugar limit! Pick another fruit: Grape, Strawberry, Blueberry, Grapefruit, Rasberry ";
-            cin >> Fruit;
-        
-        if (Math < SugarLimit)
-            cout << "Okay, enjoy your fruit! ";
+    cout << "How much sugar do you think is in this fruit? ";
+    cin >> Guess;
 
+    while (Guess != Sugar)
+    {
+        cout << "Thats wrong! Try again! " << endl;
+        cout << "New guess: ";
+        cin >> Guess2;
+
+            if (Guess2 == Sugar)
+            cout << "That's correct! You win nothing! ";
+            break;
+
+            if (Guess2 != Sugar)
+            continue;
+    }
+
+    while (Guess == Sugar)
+    {
+        cout << "That's correct! You win nothing!";
+        break;
+    }
 
 return 0;
 }
